fc5cdb2cefe7bb2cc24196828d9df8639807aa62
Jono Hill <jono@hillnz.com>
2021-10-28 19:11:30 +1300

    Add running check

--- monitor.sh
+++ monitor.sh
@@ -2,7 +2,17 @@
 
 set -e
 
+run() {
+    if [ "$running" = true ]; then
+        echo "Already running"
+    else
+        running=true
+        "$@" || true
+        running=false
+    fi
+}
+
 while true; do 
     echo -ne "HTTP/1.0 200 OK\r\n\r\n" | nc -Nl -p "$LISTEN_PORT"
-    "$@" &
+    run "$@" &
 done
